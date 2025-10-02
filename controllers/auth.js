7f6494da1350136c16ee6094d9ad134c47686b6f
Balqeesalawi <balqeesalawi195@gmail.com>
2025-10-02 10:59:39 +0300

    added signUp page and functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,25 @@
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
+
+exports.auth_signup_get = async (req, res) => {
+    res.render("auth/sign-up.ejs")
+}
+
+exports.auth_signup_post = async (req, res) => {
+    const userInDatabase = await User.findOne({username: req.body.username})
+    if(userInDatabase){
+        return res.send("Username already taken")
+    }
+
+    if(req.body.password !== req.body.confirmPassword){
+        return res.send("Password and confirm password must match")
+    }
+
+    // Password Encryption
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassword
+
+    // Register the user
+    const user = await User.create(req.body)
+    res.send(`Thanks for signing up ${user.username}`)
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 
+const authCtrl = require('../controllers/auth')
 
+router.get('/sign-up', authCtrl.auth_signup_get)
+router.post("/sign-up", authCtrl.auth_signup_post)
 
 module.exports = router
