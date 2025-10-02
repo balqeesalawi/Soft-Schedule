3155d408ebbb85064ccd3d674374b4cddae5ccb7
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-10-02 11:00:18 +0300

    navbar complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,11 @@
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
+
+
+//APi's
+
+
+ exports.auth_signout_get = async (req, res)=>{
+  req.session.destroy()
+  res.redirect("/auth/sign-in")
+ }
