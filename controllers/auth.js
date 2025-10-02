270facfad23b5a1b739fe2741151a39c795fd76c
HawraAlaam <hawraalaam@gmail.com>
2025-10-02 11:00:00 +0300

    add signin page and functionallity

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,29 @@
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
+
+exports.auth_signin_get = async (req, res) => {
+  res.render("auth/sign-in.ejs");
+}
+
+exports.auth_signin_post = async (req, res) => {
+  const userInDatabase = await User.findOne({ email: req.body.email })
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again later...")
+  }
+
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  )
+  if (!validPassword) {
+    return res.send("Login failed. Please try again later...")
+  }
+
+  // Initialize Session
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id,
+  }
+
+  res.redirect("/")
+}
--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-const express=require("express")
-const app=express()
-const morgan=require("morgan")
+const express = require("express")
+const app = express()
+
+const morgan = require("morgan")
 app.use(morgan("dev"))
-const methodOverride=require("method-override")
+
+const methodOverride = require("method-override")
 app.use(methodOverride("_method"))
-require('dotenv').config()
+
+require("dotenv").config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-const session=require("express-session")
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
-const mongoose = require('./config/db')
-app.get('/', (req, res) => {
+const session = require("express-session")
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+
+const passUsser = require("./middleware/passUser")
+const isSignedIn = require("./middleware/is-signed-in")
+app.use(passUsser)
+app.use((req, res, next) => {
+  res.locals.user = req.session.user
+  next()
+})
+
+const mongoose = require("./config/db")
+
+app.get("/", (req, res) => {
   res.render("index.ejs")
 })
+
+const authRouter = require("./routes/auth.js")
+app.use("/auth", authRouter)
+
 const port = process.env.PORT ? process.env.PORT : 3000
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log("The app is listening")
 })
