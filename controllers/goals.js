276aded024ce5c54fe857c0edf8e0edb2725ff04
HawraAlaam <hawraalaam@gmail.com>
2025-10-03 19:47:16 +0300

    add filter function

--- controllers/goals.js
+++ controllers/goals.js
@@ -13,7 +13,20 @@ exports.goals_create_post = async (req, res) => {
   const newGoal = await Goal.create({
     goal: req.body.goal,
     duration: req.body.duration,
-    owner: req.session.user._id
+    owner: req.session.user._id,
   })
   res.redirect("/goals")
+}
+
+exports.goals_filter_get = async (req, res) => {
+  const { duration } = req.query 
+
+  let goals
+  if (duration) {
+    goals = await Goal.find({ duration })
+  } else {
+    goals = await Goal.find()
+  }
+
+  res.render("goals/index.ejs", { goals })
 }
--- routes/goals.js
+++ routes/goals.js
@@ -5,5 +5,6 @@ const goalsCtrl = require("../controllers/goals")
 router.get("/", goalsCtrl.goals_index_get)
 router.get("/new", goalsCtrl.goals_create_get)
 router.post("/", goalsCtrl.goals_create_post)
+router.get("/filter", goalsCtrl.goals_filter_get)
 
 module.exports = router
