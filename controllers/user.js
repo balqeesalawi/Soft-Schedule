e0ed7a8262f834aa41df3e04729b2574efcd4084
Balqeesalawi <balqeesalawi195@gmail.com>
2025-10-07 10:29:44 +0300

    put for edit password

--- controllers/user.js
+++ controllers/user.js
@@ -10,3 +10,25 @@ exports.user_edit_get = async (req, res)=> {
     const user = await User.findById(req.params.userId)
     res.render('user/editPassword.ejs', {user})
 } 
+
+exports.user_edit_post = async (req, res)=> {
+    const user = await User.findById(req.params.userId)
+    if(!user){
+        return res.send('No user with that ID exists!')
+    }
+    const validPassword = bcrypt.compareSync(
+        req.body.oldPassword,
+        user.password
+    )
+    if (!validPassword) {
+        return res.send('Your old Password was not correct! Please try again')
+    }
+    if (req.body.newPassword !== req.body.confirmPassword){
+        return res.send('Password and confirm password must match')
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.newPassword, 10)
+    user.password = hashedPassword
+    await user.save()
+    res.send(`Your password has been updated, ${user.username}!`)
+    res.redirect(`/user/${req.params.userId}`)
+}
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userCtrl = require('../controllers/user.js')
 
 router.get('/:userId', userCtrl.user_show_get)
 router.get('/:userId/editPassword', userCtrl.user_edit_get)
+router.put('/:userId', userCtrl.user_edit_post)
 
 module.exports = router
