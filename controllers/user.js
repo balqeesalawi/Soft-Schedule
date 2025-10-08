f31877ebafd920d25f0b732c6aae964f856185c8
Balqeesalawi <balqeesalawi195@gmail.com>
2025-10-08 22:23:11 +0300

    add update username functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,19 @@
 const User = require("../models/user.js")
 const bcrypt = require('bcrypt')
 
+// to show the profile page
 exports.user_show_get = async (req, res)=> {
     const user = await User.findById(req.params.userId)
     res.render("user/profile.ejs", {user})
 }
 
+// to show form for editing password
 exports.user_edit_get = async (req, res)=> {
     const user = await User.findById(req.params.userId)
     res.render('user/editPassword.ejs', {user})
 }
 
+// to post edit password form
 exports.user_edit_post = async (req, res)=> {
     const user = await User.findById(req.params.userId)
     if(!user){
@@ -29,12 +32,11 @@ exports.user_edit_post = async (req, res)=> {
     const hashedPassword = bcrypt.hashSync(req.body.newPassword, 10)
     user.password = hashedPassword
     await user.save()
-    res.send(`Your password has been updated, ${user.username}!`)
-    res.redirect(`/user/${req.params.userId}`)
+    return res.send(`Your password has been updated, ${user.username}!`)
 }
-exports.user_profile_get = async (req,res) => {
-  res.render("user/profile.ejs")
-}
+
+
+// for uploading profile photo form 
 exports.user_profile_post = async (req,res) => {
 if (req.file){
     req.body.picture = `/uploads/${req.file.filename}`
@@ -43,3 +45,16 @@ await User.findByIdAndUpdate(req.params.userId, req.body)
     req.session.user.picture = req.body.picture
     res.redirect(`/user/${req.params.userId}/profile`)
 }
+
+exports.user_name_put = async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username })
+  if (userInDatabase) {
+    return res.send("Username already taken")
+  }
+
+
+    const user = await User.findById(req.params.userId)
+    user.username = req.body.username
+    await user.save()
+    res.redirect(`/user/${req.params.userId}/profile`)
+} 
